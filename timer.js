f81788dfa30435f304e65e0d9aa6bf0d7bba8b95
Aswin M Guptha <aswinmguptha@gmail.com>
2025-04-02 22:01:49 +0530

    Update timer.js

--- timer.js
+++ timer.js
@@ -8,7 +8,7 @@ function updateCountdown() {
     const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeLeft % (1000 * 60 * 60 * 60)) / (1000));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / (1000));
     
 
     // Add leading zeros
